84b4a2914a75709b49f7381f811087ccb0ca5bbe
Faisal Rehman <63219842+f81773@users.noreply.github.com>
2024-02-17 01:53:33 +0500

    Update Streamlitapp.py

--- Streamlitapp.py
+++ Streamlitapp.py
@@ -69,7 +69,8 @@ def predict_sentiment(urdu_string):
     # Apply TF-IDF Vectorization
     max_feature_num = 50000
     vectorizer = TfidfVectorizer(max_features=max_feature_num)
-    new_test_vecs = vectorizer.fit_transform(df_new['lemmatized_text'])
+    
+    new_test_vecs = TfidfVectorizer(max_features=max_feature_num, vocabulary=vectorizer.vocabulary_).fit_transform(df_new['lemmatized_text'])
   
 
     # Store the new vectorized text in a new variable
